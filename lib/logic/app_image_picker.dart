d3fc7fdfb63040e9b6969605abde98c23fe45ef7
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-11-03 00:35:32 +0700

    update app image picker

--- lib/logic/app_image_picker.dart
+++ lib/logic/app_image_picker.dart
@@ -7,29 +7,77 @@ import 'package:image_picker/image_picker.dart';
 class AppImagePickerService {
   final ImagePicker _picker = ImagePicker();
 
-  Future<XFile?> getImage() async {
-    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-    return image;
-  }
-
-  Future<String?> getImageAsBase64() async {
+  Future<XFile?> getImage({
+    ImageSource imageSource = ImageSource.gallery,
+    CameraDevice preferredCameraDevice = CameraDevice.rear,
+  }) async {
     try {
       final XFile? image = await _picker.pickImage(
-        source: ImageSource.gallery,
+        source: imageSource,
+        preferredCameraDevice: preferredCameraDevice,
         imageQuality: 10,
       );
       File fileFormat = File(image!.path);
 
-      // Calculate the size in MB
-      int sizeInBytes = await fileFormat.length();
-      double sizeInMb = sizeInBytes / (1024 * 1024);
-      AppLogger.debugLog("Image Size MB: $sizeInMb");
-      AppLogger.debugLog("Image Size KB: ${sizeInMb*1000}");
+      await calculateSize(fileFormat);
+
+      return image;
+    } catch (e) {
+      AppLogger.debugLog("[AppImagePickerService][getImage] $e");
+      return null;
+    }
+  }
+
+  Future<String?> getImageAsBase64({
+    ImageSource imageSource = ImageSource.gallery,
+    CameraDevice preferredCameraDevice = CameraDevice.rear,
+  }) async {
+    try {
+      final XFile? image = await _picker.pickImage(
+        source: imageSource,
+        preferredCameraDevice: preferredCameraDevice,
+        imageQuality: 10,
+      );
+      File fileFormat = File(image!.path);
+
+      await calculateSize(fileFormat);
 
       String base64Image = base64Encode(fileFormat.readAsBytesSync());
       return base64Image;
     } catch (e) {
+      AppLogger.debugLog("[AppImagePickerService][getImageAsBase64] $e");
       return null;
     }
+  }
+
+  Future<File?> getImageAsFile({
+    ImageSource imageSource = ImageSource.gallery,
+    CameraDevice preferredCameraDevice = CameraDevice.rear,
+  }) async {
+    try {
+      final XFile? image = await _picker.pickImage(
+        source: imageSource,
+        preferredCameraDevice: preferredCameraDevice,
+        imageQuality: 10,
+        maxHeight: 400,
+        maxWidth: 400,
+      );
+      File fileFormat = File(image!.path);
+
+      await calculateSize(fileFormat);
+
+      return fileFormat;
+    } catch (e) {
+      AppLogger.debugLog("[AppImagePickerService][getImageAsFile] $e");
+      return null;
+    }
+  }
+
+  Future<void> calculateSize(File fileFormat) async {
+    // Calculate the size in MB
+    int sizeInBytes = await fileFormat.length();
+    double sizeInMb = sizeInBytes / (1024 * 1024);
+    AppLogger.debugLog("Image Size MB: $sizeInMb");
+    AppLogger.debugLog("Image Size KB: ${sizeInMb * 1000}");
   }
 }
