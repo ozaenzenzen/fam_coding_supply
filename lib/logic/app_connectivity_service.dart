10703ac02ca6f2d69c484335defe2a2ad1812882
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-11-04 07:36:40 +0700

    update

--- lib/logic/app_connectivity_service.dart
+++ lib/logic/app_connectivity_service.dart
@@ -3,9 +3,9 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 enum AppConnectivityStatus { wifi, cellular, offline }
 
 class AppConnectivityService {
-  static AppConnectivityStatus connectionStatus = AppConnectivityStatus.offline;
+  AppConnectivityStatus connectionStatus = AppConnectivityStatus.offline;
 
-  static Future<void> init() async {
+  Future<void> init() async {
     connectionStatus = _connectionStatus(await Connectivity().checkConnectivity());
     Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
       connectionStatus = _connectionStatus(result);
@@ -13,7 +13,7 @@ class AppConnectivityService {
   }
 
   /// Convert from the third part enum to our own enum
-  static AppConnectivityStatus _connectionStatus(ConnectivityResult result) {
+  AppConnectivityStatus _connectionStatus(ConnectivityResult result) {
     switch (result) {
       case ConnectivityResult.mobile:
         return AppConnectivityStatus.cellular;
