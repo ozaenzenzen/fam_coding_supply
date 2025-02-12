6950ab027d1afb89676ac15879f5285130ec512c
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-02-12 12:24:56 +0700

    export flutter screen util

--- lib/fam_coding_supply.dart
+++ lib/fam_coding_supply.dart
@@ -18,6 +18,7 @@ export 'package:google_fonts/google_fonts.dart';
 export 'package:super_tooltip/super_tooltip.dart';
 export 'package:intl/intl.dart';
 export 'package:dio/dio.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FamCodingSupply {
   AppConnectivityServiceCS appConnectivityService = AppConnectivityServiceCS();
