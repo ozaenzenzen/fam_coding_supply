0bec9534c2f42be09b99f6d05b42baf80423e4ca
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-02-27 11:12:13 +0700

    update app logger

--- lib/logic/app_logger.dart
+++ lib/logic/app_logger.dart
@@ -1,16 +1,45 @@
 import 'dart:developer';
 
+import 'package:flutter/foundation.dart';
+
 class AppLoggerCS {
   static void debugLog(
     String value, {
     bool isActive = true,
   }) {
-    if (useLogger != null && useLogger == true) {
+    // if (useLogger != null && useLogger == true) {
+    if (useLogger) {
       if (isActive) {
-        log(value);
+        if (useFoundation) {
+          debugPrint(value);
+        } else {
+          log(value);
+        }
       }
     }
   }
 
-  static bool? useLogger;
+  static void debugLogSpecific({
+    required String value,
+    required String processName,
+    required String functionName,
+    bool isActive = true,
+  }) {
+    // if (useLogger != null && useLogger == true) {
+    if (useLogger) {
+      if (isActive) {
+        if (useFoundation) {
+          debugPrint("[$functionName][$processName]: $value");
+        } else {
+          log("[$functionName][$processName]: $value");
+        }
+      }
+    }
+  }
+
+  /// WARNING!!! THIS SHOULD BE FALSE ON PRODUCTION
+  static bool useFoundation = false;
+
+  /// WARNING!!! THIS SHOULD BE FALSE ON PRODUCTION
+  static bool useLogger = false;
 }
