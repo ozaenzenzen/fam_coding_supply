c1707fcd0a5543a5c9b080d79d28e6daabcbd550
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-04-15 20:52:47 +0700

    add websocket

---
+++ lib/logic/app_websocket_service.dart
@@ -0,0 +1,53 @@
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class AppWebSocketServiceCS {
+  Future<WebSocketChannel?> connect(String wsUrl) async {
+    try {
+      // final url = Uri.parse('ws://example.com');
+      final url = Uri.parse(wsUrl);
+      final channel = WebSocketChannel.connect(url);
+      await channel.ready;
+
+      // channel.stream.listen((message) {
+      //   channel.sink.add('received!');
+      //   channel.sink.close(status.goingAway);
+      // });
+
+      return channel;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<IOWebSocketChannel?> connect2(
+    String wsUrl, {
+    Iterable<String>? protocols,
+    Map<String, dynamic>? headers,
+    Duration? pingInterval,
+    Duration? connectTimeout,
+    // HttpClient? customClient,
+  }) async {
+    try {
+      // final url = Uri.parse('ws://example.com');
+      final url = Uri.parse(wsUrl);
+      final channel = IOWebSocketChannel.connect(
+        url,
+        protocols: protocols,
+        headers: headers,
+        pingInterval: pingInterval,
+        connectTimeout: connectTimeout,
+      );
+      await channel.ready;
+
+      // channel.stream.listen((message) {
+      //   channel.sink.add('received!');
+      //   channel.sink.close(status.goingAway);
+      // });
+
+      return channel;
+    } catch (e) {
+      return null;
+    }
+  }
+}
